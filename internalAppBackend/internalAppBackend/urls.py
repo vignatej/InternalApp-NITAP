4d5df1311e7d7760613b425f7364136f42d68433
vigna tej <vignatej123@gmail.com>
2023-03-11 15:28:26 +0530

    secondCommit

---
+++ internalAppBackend/internalAppBackend/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    
+    path('admin/', admin.site.urls),
+    path('',include('placementStories.urls')),
+    path('users/',include('users.urls')),
+    path('chat/', include('chat.urls')),
+]
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
